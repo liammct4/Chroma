0aa712d32742a1d56319bf9010273c2f768cb8e9
Liam M <lmcrailt4@gmail.com>
2022-08-02 23:18:06 +0100

    Restructured project to implement MVVM.

---
+++ Chroma/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chroma.ViewModel
+{
+	/// <summary>
+	/// View model for a <see cref="MainWindow"/>.
+	/// </summary>
+	public class MainWindowViewModel : ViewModelBase
+	{
+
+	}
+}
---
+++ Chroma/ViewModel/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Chroma.ViewModel
+{
+	/// <summary>
+	/// Base class for view model classes, cannot be instantiated.
+	/// </summary>
+	public abstract class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+		/// <summary>
+		/// Used to notify UI of binded property change. Call when binded property has been changed.
+		/// </summary>
+		/// <param name="propertyName">Name of property</param>
+		public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new(propertyName));
+	}
+}
