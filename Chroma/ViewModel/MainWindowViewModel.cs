e0cb56fde7d765ba8213f0660b94aee8fbc1d5e1
Liam M <lmcrailt4@gmail.com>
2022-08-03 16:22:19 +0100

    Added basic ui layout.

--- Chroma/ViewModel/MainWindowViewModel.cs
+++ Chroma/ViewModel/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Chroma.ViewModel
 {
@@ -11,6 +12,30 @@ namespace Chroma.ViewModel
 	/// </summary>
 	public class MainWindowViewModel : ViewModelBase
 	{
-
+		public Color CurrentColour
+		{
+			get => _currentColour;
+			set
+			{
+				_currentColour = value;
+				OnPropertyChanged(nameof(CurrentColour));
+			}
+		}
+		private Color _currentColour;
+		public int R
+		{
+			get => CurrentColour.R;
+			set => CurrentColour = Color.FromRgb((byte)value, CurrentColour.G, CurrentColour.B);
+		}
+		public int G
+		{
+			get => CurrentColour.G;
+			set => CurrentColour = Color.FromRgb(CurrentColour.R, (byte)value, CurrentColour.B);
+		}
+		public int B
+		{
+			get => CurrentColour.B;
+			set => CurrentColour = Color.FromRgb(CurrentColour.R, CurrentColour.G, (byte)value);
+		}
 	}
 }
